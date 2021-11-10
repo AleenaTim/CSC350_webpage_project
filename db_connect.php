f4e53c389b2ee1621dda860ab8d8fa8458297da1
aleena.t <38635405+AleenaTim@users.noreply.github.com>
2021-11-10 01:25:44 -0500

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,10 @@
 <?php 
 
-	mysql://
-	b3fe0f59ce8455
-	:
-	c18bd9b8@us-cdbr-east-04.cleardb.com
-	/heroku_2dceaae5180431c?reconnect=true
-?>
+	// connect to the database
+	$con = mysqli_connect('dbconnect', 'root', '', 'occss');
+
+	// check connection
+	if(!$con){
+		echo 'Connection error: '. mysqli_connect_error();
+	}
+
