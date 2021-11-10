08c51764aa01662c2008988fecf73ddc8a1f8152
aleena.t <38635405+AleenaTim@users.noreply.github.com>
2021-11-09 23:32:13 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,7 +1,7 @@
 <?php
 	
-	include('C:\xampp\htdocs\templates\csc350project\db_connect.php');			
-	include('C:\xampp\htdocs\templates\csc350project\header.php');
+	include('db_connect.php');			
+	include('header.php');
 	$firstName = $_SESSION['firstName'];
 	$lastName = $_SESSION['lastName'];
 	$total = $_SESSION['total'];
